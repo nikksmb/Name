9c70e03c57bc4ae36785513ecf55953620e8a2fb
nikksmb <nikksmb@yandex.ru>
2014-09-16 21:44:09 +0200

    Lab1, OOP
    ...

---
+++ Lab1_OOP/ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+    public abstract class ShapeC
+    {
+        protected int x1,y1,x2,y2;
+        public abstract void Draw();
+    }
---
+++ Lab1_OOP/EllipseC/EllipseC/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+public class EllipseC : ShapeC
+{
+    // private int x1, y1, x2, y2;
+    public EllipseC(int a, int b, int c, int d)
+    {
+        x1 = a;
+        y1 = b;
+        x2 = c;
+        y2 = d;
+    }
+    public override void Draw()
+    {
+        System.Drawing.Pen myPen;
+        myPen = new System.Drawing.Pen(System.Drawing.Color.Red);
+        System.Drawing.Graphics formGraphics = Form.ActiveForm.CreateGraphics();
+        formGraphics.DrawEllipse(myPen, x1, y1, x2, y2);
+        myPen.Dispose();
+        formGraphics.Dispose();
+    }
+}
